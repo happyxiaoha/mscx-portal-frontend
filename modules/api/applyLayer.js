a4e75a87424f459039ee68d4e47d6270c8a2a021
Tang Heng <846771475@qq.com>
2016-12-14 20:36:02 +0800

    API详情线下洽谈弹层

--- modules/api/applyLayer.js
+++ modules/api/applyLayer.js
@@ -9,17 +9,17 @@ var packageModel = Backbone.Model.extend({
 
 var view = Backbone.View.extend({
     tagName: 'div',
-    id: 'applyBox',
-    className: 'hide',
-    template: _.template(template),
+    className: 'apply-box hide',
+    template: _.template(template, {variable: 'data'}),
     events: {
-        'change .number': 'changeTotal',
+        'change .number': 'changeTdTotal',
         'click input[type="checkbox"]': 'selectPackage'
     },
     initialize: function() {
         this.packageModel = new packageModel();
 
         this.listenTo(this.packageModel, 'sync', this.render);
+        this.on('caculate', this.caculate);
 
         this.packageModel.fetch({
             data: {
@@ -28,13 +28,15 @@ var view = Backbone.View.extend({
         })
     },
     render: function() {
-
+        this.$el.html(this.template(this.packageModel.toJSON()));
+        this.trigger('caculate');
     },
-    changeTotal: function(event) {
+    changeTdTotal: function(event) {
         var $target = this.$(event.currentTarget);
         var $tr = $target.parents('tr');
-        var num = $target.val();
-        var price = $target.data('price');
+        var $number = $tr.find('.number');
+        var num = $number.val();
+        var price = $number.data('price');
 
         if(num < 0) {
             $target.val(0);
@@ -42,16 +44,39 @@ var view = Backbone.View.extend({
         }
 
         $tr.find('.total').html(num * price);
+
+        this.trigger('caculate');
     },
     selectPackage: function(event) {
         var $target = this.$(event.currentTarget);
         var $tr = $target.parents('tr');
 
-        if($target[0].checked) {
-
-        }else {
+        if(!$target[0].checked) {
             $tr.find('.number').val(0);
+            $tr.find('.total').html(0);
+        }else {
+            $tr.find('.number').val(1).change();
         }
+
+        this.trigger('caculate');
+    },
+    caculate: function() {
+        var count = 0,
+            totalPrice = 0;
+
+        this.$el.find('td input[type="checkbox"]:checked').each(function(index, item) {
+            var $tr = $(item).parents('tr'),
+                $number = $tr.find('.number'),
+                num = $number.val(),
+                price = $number.data('price');
+
+            count++;
+            totalPrice += num * price;
+        });
+
+        this.$el.find('#selectedCount').html(count);
+        this.$el.find('#totalPirce').html(totalPrice);
+        
     }
 });
 

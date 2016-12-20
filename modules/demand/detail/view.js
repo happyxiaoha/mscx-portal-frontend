153fd02b25ff2be461a02d3ebbabed6479b71aec
Tang Heng <846771475@qq.com>
2016-12-20 11:15:08 +0800

    util放置到lib中

--- modules/demand/detail/view.js
+++ modules/demand/detail/view.js
@@ -1,13 +1,24 @@
 'use strict';
 
 var template = require('html!./template.html');
+var detailModel = Backbone.Model.extend({
+    url: mscxPage.host + '/ro/mscx-requirement-api/apiDetail.do'
+})
 require('../demand.css');
 
 var view = Backbone.View.extend({
     el: mscxPage.domEl.demandEl,
     template: _.template(template, {variable: 'data'}),
     initialize: function() {
-        this.$el.addClass('grid960 bgWhite boxShadiow').html()
+        this.$el.addClass('ReleaseMainCons grid960 clearfix bgWhite boxShadiow').html(this.template());
+        
+        this.detailModel = new detailModel();
+
+        this.detailModel.fetch({
+            data: {
+                id: this.id
+            }
+        })
     }
 });
 

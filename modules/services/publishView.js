90d3f71b6e5075c5abafe8218222b678d43a4ae1
Tang Heng <846771475@qq.com>
2016-12-24 14:38:52 +0800

    完成微服务发布功能

--- modules/services/publishView.js
+++ modules/services/publishView.js
@@ -3,8 +3,10 @@
  */
 
 var template = require('html!./publishTemplate.html');
+var tagView = require('./tagsLayer.js');
 require('./services.css');
 require('validate');
+require('formAjax');
 require('util');
 
 // 获取微信详情
@@ -21,12 +23,11 @@ var categoryModel = Backbone.Model.extend({
 })
 // 微服务标签
 var tagModel = Backbone.Model.extend({
-    url: mscxPage.request.dict + 'category/getServiceCategory.do'
+    url: mscxPage.request.dict + 'tag/getTagsInfo.do'
 })
 // 上传图片
-var uploadImgModle = Backbone.Model.extend({
-    url: mscxPage.request.app + '/pic/upload.do'
-});
+var uploadImgUrl = mscxPage.request.app + 'pic/upload.do';
+
 var addModel = Backbone.Model.extend({
     idAttribute: 'addId',
     url: mscxPage.request.app + 'apply.do'
@@ -39,28 +40,35 @@ var modifyModel = Backbone.Model.extend({
 var createDemandView = Backbone.View.extend({
     el: mscxPage.domEl.apiEl,
     events: {
-        
+        'change .upload-file': 'doUploadImg',
+        'click #selectTagBtn': 'getTags',
+        'change #selectCategory': 'saveCategory'
     },
     template: _.template(template, {variable: 'data'}),
     initialize: function() {
-        // 如果有ID则说明是进入修改页面
-        this.detailModel = new detailModel();
-        if(this.id) {
-            this.listenTo(this.detailModel, 'sync', this.renderDetail);
-            this.detailModel.fetch({
-                data: {
-                    id: this.id
-                }
-            })
-        }else {
-            this.renderDetail();
-        }
-
         this.model = this.id ? new modifyModel() : new addModel();
         this.objectModel = new objectModel();
         this.categoryModel = new categoryModel();
         this.tagModel = new tagModel();
+        // 如果有ID则说明是进入修改页面
+        this.detailModel = new detailModel();
+
+        this.tags = new Backbone.Model();
+        
         this.listenTo(this.model, 'sync', this.handleSubmit);
+        this.listenTo(this.tagModel, 'sync', this.showTagLayer);
+        this.listenTo(this.tags, 'change', this.fillTags);
+        this.on('renderDetail', this.renderDetail);
+
+        var queue = [this.objectModel.fetch(), this.categoryModel.fetch()];
+
+        if(this.id) {
+            queue.push(this.detailModel.fetch({data: {id: this.id}}));
+        }
+
+        $.when.apply($, queue).done(function() {
+            this.trigger('renderDetail');
+        }.bind(this));
     },
     validateConfig: function () {
         var me = this;
@@ -106,6 +114,7 @@ var createDemandView = Backbone.View.extend({
     submitForm: function () {
         var params = this.$form.serializeObject();
 
+        params.serviceObject = params.serviceObject.join(',');
         this.model.set(params);
         this.model.save();
     },
@@ -120,24 +129,106 @@ var createDemandView = Backbone.View.extend({
             })
         }
     },
-    changeDate: function(event) {
-        var $target = this.$(event.currentTarget);
-        var date = $target.val();
-
-        $target.trigger('keyup');
-
-        this.model.set({
-            endTime: date
-        })
-    },
     renderDetail: function() {
-        var model = this.detailModel.toJSON();
+        var params = {};
+        var detail = this.detailModel.toJSON();
+        var object = this.objectModel.toJSON();
+        var category = this.categoryModel.toJSON();
 
-        this.$el.html(this.template(model.result));
+        _.extend(params, {
+            detail: detail.result,
+            objects: object.result,
+            category: category.result,
+        })
 
-        this.$form = this.$('form');
+        this.$el.html(this.template(params));
+
+        this.$form = this.$('#publishForm');
+        this.$uploadIcon = this.$('#uploadServiceIcon');
+        this.$uploadDemoIcon1 = this.$('#uploadDemoIcon1');
+        this.$uploadDemoIcon2 = this.$('#uploadDemoIcon2');
+        this.$uploadDemoIcon3 = this.$('#uploadDemoIcon3');
         this.$form.validate(this.validateConfig());
 
+        this.currentCategory = this.$('#selectCategory').val();
+    },
+    doUploadImg: function(event) {
+        var me = this;
+        var id = event.target.id;
+        var form;
+
+        switch(id) {
+            case 'service':
+                form = this.$uploadIcon;
+                break;
+            case 'demo1':
+                form = this.$uploadDemoIcon1;
+                break;
+            case 'demo2':
+                form = this.$uploadDemoIcon2;
+                break;
+            case 'demo3':
+                form = this.$uploadDemoIcon3;
+                break;
+        }
+
+        form.ajaxSubmit({
+            url: uploadImgUrl,
+            success: function(res) {
+                var params = res.result;
+                if(res.status == 'ERROR') {
+                    layer.msg(res.message);
+                }else {
+                    me.$('#' + id + 'Icon').attr('src', params.uri);
+                    me.$('#' + id + 'Key').val(params.key);
+                }
+            },
+            error: function() {
+                layer.msg('上传失败');
+            }
+        })
+    },
+    showTagLayer: function(event) {
+        var tags = this.tagModel.toJSON();
+        var me = this;
+
+        this.tagView = new tagView({
+            model: tags
+        });
+        this.$el.append(this.tagView.$el);
+
+        this.tagView.delegate = this;
+
+        this.layer = layer.open({
+            type: 1,
+            btn: ['取消'],
+            title: '服务标签',
+            shade: 0.6,
+            shadeClose: true,
+            area: ['500px'],
+            content: this.tagView.$el,
+            btn1: function (index) {
+                layer.close(index);
+            },
+            end: function() {
+                me.tagView.remove();
+            }
+        })
+    },
+    getTags: function(event) {
+        event.preventDefault();
+        this.tagModel.fetch({
+            data: {
+                categoryId: this.currentCategory
+            }
+        })
+    },
+    saveCategory: function() {
+        this.currentCategory = this.$('#selectCategory').val();
+    },
+    fillTags: function() {
+        this.$('#tagId').val(this.tags.get('tagId'));
+        this.$('#tagName').val(this.tags.get('tagName'));
     }
 });
 

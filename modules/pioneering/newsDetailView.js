d95817b319ade76695d7b0a388b9607d50e56e7d
Tang Heng <846771475@qq.com>
2016-12-27 19:08:26 +0800

    创业园地新闻详情页面新增分享

--- modules/pioneering/newsDetailView.js
+++ modules/pioneering/newsDetailView.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var shareView = require('shareWidget/shareView.js');
 require('./pioneering.css');
 
 var view = Backbone.View.extend({
@@ -9,8 +10,14 @@ var view = Backbone.View.extend({
 
         var me = this;
 
+        this.shareView = new shareView({
+            className: 'share posAB'
+        });
+
         this.$el.load(this.model.url + '?time=' + +(new Date()), function() {
             me.$el.removeClass('opacity0');
+            // 添加分享组件
+            me.$('.TopTit').addClass('pr').append(me.shareView.$el);
         });
 
         return this;

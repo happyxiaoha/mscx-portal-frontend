89b447b89ef84fb98fb4fb43ba5ace3508b77fd6
Tang Heng <846771475@qq.com>
2016-12-29 09:50:34 +0800

    未登录时头部name报错修复

--- widget/headerWidget/headerView.js
+++ widget/headerWidget/headerView.js
@@ -72,7 +72,7 @@ var headerView = Backbone.View.extend({
         this.$el.html(this.template({
             id: this.id,
             menuList: menuList,
-            username: nJson.result.name
+            username: nJson.result && nJson.result.name
         }));
         var _c;
         $("#personReal").hover(function(){

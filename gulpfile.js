740f33f3111e1a6b5bb4c689ba6ea5eca97274c5
qinwc <qinwc@dc.com>
2017-01-03 16:47:16 +0800

    添加api帮助 新手指导 修改判断是否是ie的方式

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ gulp.task('backboneBuild', function() {
 });
 
 gulp.task('copy', function() {
-    return gulp.src(['./css/**/*', './*.html', './lib/**/*', './images/newicon/ic_newlogo.png'], {base: './'})
+    return gulp.src(['./css/**/*', './*.html', './lib/**/*', './images/newicon/ic_newlogo.png', './images/apihelp/*', './images/guidance/*'], {base: './'})
         .pipe(gulp.dest('build'));
 });
 

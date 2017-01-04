a5145275811fdd1dd18f7c0e2365f0df925f2994
顾佳书 <jiashu9123870@126.com>
2017-01-04 19:28:51 +0800

    api无comments

--- modules/userInfo/api/apiView.js
+++ modules/userInfo/api/apiView.js
@@ -150,7 +150,8 @@ var apiView = Backbone.View.extend({
         var $this = $(e.target).closest('tr'),
             index = $this.index(),
             res = this.model.get('result').list;
-        layer.alert(res[index].comments,{title:'拒绝原因'});
+        var sComments = res[index].comments || '-';
+        layer.alert(sComments,{title:'拒绝原因'});
     },
     buildPackageTable: function () {
         var res = this.getPackageModel.get('result');

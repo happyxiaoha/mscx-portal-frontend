8310209dc89da9185c0c9c4e2eb5db13cf164627
顾佳书 <jiashu9123870@126.com>
2017-01-11 16:50:28 +0800

    新增服务套餐

--- modules/services/desView.js
+++ modules/services/desView.js
@@ -3,14 +3,18 @@
  */
 
 var template = require('html!./desTemplate.html');
+var packageTabletemplate = require('html!./packageTableTemplate.html');
 require('./services.css');
 
 require('util');
 
 var detailModel = Backbone.Model.extend({
     url: mscxPage.request.app + 'publish/get.do'
-})
-
+});
+// 获取套餐信息
+var detailChargeModel = Backbone.Model.extend({
+    url: mscxPage.request.app + 'publish/chargeRule/get.do'
+});
 var view = Backbone.View.extend({
     el: mscxPage.domEl.apiEl,
     events: {
@@ -26,10 +30,33 @@ var view = Backbone.View.extend({
             });
         this.listenTo(this.detailModel, 'sync', this.renderDes);
     },
-
+    renderPackageUpdate: function (appId) {
+        var that = this;
+        new detailChargeModel().fetch({
+            data: {
+                'appId': appId
+            },
+            'success': function (model) {
+                that.chargeRule = model.get('result');
+                that.buildChargeTable();
+            }
+        })
+    },
+    buildChargeTable: function () {
+        var chargeSetJson = this.chargeRule || [];
+        if(chargeSetJson.length > 0){
+            $('.package-error').hide();
+        }
+        var packageTableTemps = _.template(packageTabletemplate);
+        $('#packageTable').html(packageTableTemps({chargeSetJson: chargeSetJson,isUpdate: true}));
+    },
     renderDes: function(res) {
-        var model = res.toJSON();
-        this.$el.html(this.template(model.result));
+        var model = res.toJSON(),
+            res = model.result;
+        if(res.chargeType == '02'){
+            this.renderPackageUpdate(res.id);
+        }
+        this.$el.html(this.template(res));
     }
 });
 
---
+++ widget/packageWidget/packageItemView.js
@@ -0,0 +1,177 @@
+'use strict';
+var template = require('html!./packageItem.html');
+require('validate');
+var getFeeModel = Backbone.Model.extend({
+    url: mscxPage.request.app + 'calEarning.do'
+});
+require('./packageItem.css');
+
+var view = Backbone.View.extend({
+    events: {
+        'blur #price': 'limitPriceFun',
+        'blur #chargeCount': 'disChargeCount'
+    },
+    packageValidateConfig: function () {
+        var that = this;
+        return {
+            rules: {
+                name: {
+                    required: true,
+                    maxlength: 50
+                },
+                price: {
+                    required: true,
+                    number: true,
+                    min: 0
+                },
+                chargeCount: {
+                    required: true,
+                    number: true,
+                    integers: true,
+                    min: 0
+                },
+                monthLimit: {
+                    number: true,
+                    integers: true,
+                    min: 0
+                },
+                effectDate: {
+                    required: true,
+                    date: true
+                },
+                expiryDate: {
+                    date: true
+                }
+            },
+            messages: {
+                name: {
+                    maxlength: '套餐名称不大于50个字符'
+                },
+                chargeCount: {
+                    integers: '大于1的正整数'
+                },
+                monthLimit: {
+                    integers: '大于1的正整数'
+                }
+            },
+            submitHandler: function () {
+                that.callbackSave();
+            }
+        }
+    },
+    template: _.template(template),
+    buildData: function () {
+        function lastDay(sdata){
+            var resDate = new Date(),
+                newData = new Date();
+            if(sdata){
+                resDate = new Date(sdata);
+                newData = new Date(sdata)
+            }
+            resDate.setDate(newData.getDate()-1);
+            return resDate;
+        }
+
+        if($('#effectDate').data('daterangepicker')){
+            $('#effectDate').data('daterangepicker').setOptions({minDate: lastDay(),singleDatePicker: true,startDate: moment(),format: 'YYYY-MM-DD'})
+        }
+        else {
+            if($('#expiryDate').val()){
+                $('#effectDate').daterangepicker({
+                    format: 'YYYY-MM-DD',
+                    singleDatePicker: true,
+                    startDate: moment(),
+                    minDate: lastDay(),
+                    maxDate: lastDay($('#expiryDate').val())
+                }).on('apply.daterangepicker',function (ev,picker) {
+                    $('#expiryDate').data('daterangepicker').setOptions({'minDate': lastDay($('#effectDate').val()),singleDatePicker: true,startDate: moment()});
+                });
+            }
+            else {
+                $('#effectDate').daterangepicker({
+                    format: 'YYYY-MM-DD',
+                    singleDatePicker: true,
+                    startDate: moment(),
+                    minDate: lastDay()
+                }).on('apply.daterangepicker',function (ev,picker) {
+                    $('#expiryDate').data('daterangepicker').setOptions({'minDate': lastDay($('#effectDate').val()),singleDatePicker: true,startDate: moment()});
+                });
+            }
+        }
+        if($('#expiryDate').data('daterangepicker')){
+            $('#expiryDate').data('daterangepicker').setOptions({minDate: lastDay(),singleDatePicker: true,startDate: moment(),format: 'YYYY-MM-DD'})
+        }
+        else {
+            $('#expiryDate').daterangepicker({
+                format: 'YYYY-MM-DD',
+                singleDatePicker: true,
+                startDate: moment(),
+                minDate: lastDay($('#effectDate').val())
+            }).on('apply.daterangepicker',function (ev,picker) {
+                if($('#effectDate').length > 0) {
+                    $('#effectDate').data('daterangepicker').setOptions({
+                        'maxDate': $('#expiryDate').val(),
+                        minDate: lastDay(),
+                        singleDatePicker: true,
+                        startDate: moment()
+                    });
+                }
+            });
+        }
+    },
+    displayFeeMes: function (sVal) {
+        new getFeeModel().fetch({
+            type:'GET',
+            data: {
+                price: sVal
+            },
+            success: function(model){
+                var res = model.get('result');
+                $('.earning-count').html(res.earningCount);
+                $('.fee-count').html(res.feeCount);
+                $('.earning-percent').html(res.earningPercent*100);
+                $('.fee-percent').html(res.feePercent*100);
+            }
+        });
+    },
+    limitPriceFun: function (e) {
+        if(e.target.id == 'price'){
+            var $this = $(e.target),
+                sVal = parseFloat($.trim($this.val())),
+                $limitInput = $('input[name="boughtLimit"]');
+            if(sVal === 0) {
+                $($limitInput[0]).attr('disabled',true);
+                $($limitInput[1]).prop("checked",true)
+            }
+            else {
+                $($limitInput[0]).attr('disabled',false);
+            }
+            if(!$this.hasClass('error') && sVal > 0){
+                this.displayFeeMes(sVal);
+            }
+            $this = null;
+            $limitInput = null;
+        }
+    },
+    disChargeCount: function(e) {
+        if(e.target.id == 'chargeCount'){
+            var $this = $(e.target),
+                sVal = parseFloat($.trim($this.val()));
+            if(!$this.hasClass('error') && sVal > 0){
+                $('.charge-count').html(sVal);
+            }
+        }
+    },
+    initialize: function() {
+        this.callbackSave = this.attributes.callbackFun;
+        var res = this.model;
+        this.$el.addClass('server-package-manage').html(this.template({'res':res}));
+        if(res.price){
+            this.displayFeeMes(res.price);
+        }
+        $('#serverChargePackage').validate(this.packageValidateConfig());
+        this.buildData();
+    }
+});
+
+module.exports = view;

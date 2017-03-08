23daf8db01d91498497b9a683398bd8e0e300cb9
Tang Heng <846771475@qq.com>
2017-03-08 09:14:13 +0800

    微服务新增范围筛选

--- modules/services/contentView.js
+++ modules/services/contentView.js
@@ -17,7 +17,7 @@ var view = Backbone.View.extend({
         this.searchView = new searchView({
             id: this.id,
             model: {
-                options: ['objects', 'range', this.id + 'Category', this.id + 'Tags', 'chargeWay'],
+                options: ['objects', 'scopes', 'range', this.id + 'Category', this.id + 'Tags', 'chargeWay'],
                 defaults: this.model
             }
         });

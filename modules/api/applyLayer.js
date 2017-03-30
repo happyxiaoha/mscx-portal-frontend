29d338123a4aadabfd7179b2d39cacd62e045bba
顾佳书 <jiashu9123870@126.com>
2017-03-30 15:52:21 +0800

    计算金额带有小数时出现问题的Bug

--- modules/api/applyLayer.js
+++ modules/api/applyLayer.js
@@ -81,7 +81,7 @@ var view = Backbone.View.extend({
             num = 1;
         }
 
-        $tr.find('.total').html(num * price);
+        $tr.find('.total').html((price*10000 * num)/10000);
 
         this.trigger('caculate');
     },
@@ -107,7 +107,7 @@ var view = Backbone.View.extend({
                 price = $number.data('price');
 
             count++;
-            totalPrice += num * price;
+            totalPrice += (price*10000 * num)/10000;
         }
 
         this.amount = totalPrice;

ba035de913974ca36aeeab9385e4ac8c97b27ecf
Tang Heng <846771475@qq.com>
2017-03-30 10:05:46 +0800

    积分常见问题提交

--- modules/userInfo/point/pointQAView.js
+++ modules/userInfo/point/pointQAView.js
@@ -3,26 +3,14 @@
 var commonTemplate = require('html!./common.html');
 var template = require('html!./pointQA.html');
 
-var pointListModel = Backbone.Model.extend({
-    url: mscxPage.request.point + 'point/getAllPointRecordList.do'
-});
-
 var userView = Backbone.View.extend({
     el: mscxPage.domEl.userCenterRight,
-    events: {
-
-    },
     commonTemplate: _.template(commonTemplate),
     initialize: function() {
         this.$el.html(this.commonTemplate({
-            name: 'point'
+            name: 'pointQA'
         }));
-    },
-    render: function () {
-        
-    },
-    initRender: function () {
-        // this.$('#userInfoArea').html(template);
+        this.$('#userInfoArea').html(template);
     }
 });
 module.exports = userView;

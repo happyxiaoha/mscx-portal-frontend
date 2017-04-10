5685b5e45064a0b12c3f9ab36084f722ab4cf66b
顾佳书 <jiashu9123870@126.com>
2017-04-10 14:54:55 +0800

    我的SaaS

--- modules/userInfo/sass/mySaasView.js
+++ modules/userInfo/sass/mySaasView.js
@@ -7,26 +7,24 @@ require('./saas.css');
 require('util');
 
 var mySaaSModel = Backbone.Model.extend({
-    url: mscxPage.request.saas + 'attention/list.do'
+    url: mscxPage.request.order + 'saas/getSaaSList.do'
 });
 
 var mySaasView = Backbone.View.extend({
     el: mscxPage.domEl.userCenterRight,
     pagObj: {
-        pageSize: 5,
+        pageSize: 10,
         pageNum: 1,
         totalPage: 0
     },
-    events: {
-        'click .next-page': 'changePage'
-    },
+    events: {},
     initialize: function() {
         var that = this;
         this.$el.addClass('user-center-tap');
         this.$el.html(_.template(commonTemplate)({name:'mySaas'}));
-        /*
+        this.model = new mySaaSModel();
         this.model.on('change',function () {
-            that.renderCarArea();
+            that.render();
         });
         this.model.fetch({
             data: {
@@ -35,11 +33,37 @@ var mySaasView = Backbone.View.extend({
                 t: new Date().getTime()
             }
         });
-        */
-        this.render();
+        this.$el.find('#saasInfo').html(mySaasTemps);
+    },
+    reloadPage: function () {
+        this.model.fetch({
+            data: {
+                pageSize: this.pagObj.pageSize,
+                page: this.pagObj.pageNum
+            }
+        });
     },
     render: function () {
-        this.$el.find('#saasInfo').html(mySaasTemps);
+        var res = this.model.get('result'),
+            that = this,
+            mySaaSList = res.list,
+            page = res.page;
+        this.pagObj.pageNum = page.currentPage;
+        this.pagObj.totalPage = page.totalPage;
+        var temps = _.template($('#mySaaSListTemps').html());
+        this.$el.find('tbody').html(temps({mySaaSList: mySaaSList}));
+        laypage({
+            cont: 'mySaaSPages',
+            skip: true,
+            pages: this.pagObj.totalPage,
+            curr: this.pagObj.pageNum || 1,
+            jump: function(obj, first){
+                if(!first){
+                    that.pagObj.pageNum = obj.curr;
+                    that.reloadPage();
+                }
+            }
+        });
     }
 });
 

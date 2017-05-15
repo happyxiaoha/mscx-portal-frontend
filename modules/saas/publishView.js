026b0cac60b147196b4802bd7d4074caf14152f7
Tang Heng <846771475@qq.com>
2017-05-15 11:05:09 +0800

    发布saas服务-收费类型

--- modules/saas/publishView.js
+++ modules/saas/publishView.js
@@ -417,7 +417,7 @@ var createDemandView = Backbone.View.extend({
     changeChargeType: function (e) {
         var sId = e.target.id.replace('ct','');
         this.chargeType = sId;
-        sId == '01' ?  $('.server-package').hide() : ($('.server-package').show(),this.buildChargeTable());
+        (sId == '01' || sId == '03') ?  $('.server-package').hide() : ($('.server-package').show(),this.buildChargeTable());
         e.stopPropagation();
     },
     addChargePackage: function () {

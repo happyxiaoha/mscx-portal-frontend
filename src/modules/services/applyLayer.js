f0dccdbb4f5d1b9d0835a45fc24da5aea9301c6f
Tang Heng <846771475@qq.com>
2017-06-06 11:10:31 +0800

    fix #21934

--- src/modules/services/applyLayer.js
+++ src/modules/services/applyLayer.js
@@ -74,6 +74,7 @@ var view = Backbone.View.extend({
         var $tr = $target.parents('tr');
         var $number = $tr.find('.number');
         var num = Math.floor($number.val());
+        if(num>100000) {num = 100000;}
         $number.val(num);
         var price = $number.data('price');
 

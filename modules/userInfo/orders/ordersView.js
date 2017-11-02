4e9703833b43a9d51d904d3a592c4c7634100e41
Tang Heng <846771475@qq.com>
2017-11-02 09:52:49 +0800

    订单管理-下架的服务需求保证金充值不能支付

--- modules/userInfo/orders/ordersView.js
+++ modules/userInfo/orders/ordersView.js
@@ -16,6 +16,9 @@ var updateShopModel = Backbone.Model.extend({
 var cancelOrderModel = Backbone.Model.extend({
     url: mscxPage.request.order + 'order/cancelOrder.do'
 });
+var serviceDetailModel = Backbone.Model.extend({
+    url: mscxPage.request.demand + 'getServiceDetailOfMe.do'
+});
 
 var orderView = Backbone.View.extend({
     el: mscxPage.domEl.userCenterRight,
@@ -33,8 +36,10 @@ var orderView = Backbone.View.extend({
         this.$el.html(_.template(commonTemplate)({name:'order'}));
         this.model = new orderListModel();
         this.cancelOrderModel = new cancelOrderModel();
+        this.serviceDetailModel = new serviceDetailModel();
 
         this.listenTo(this.cancelOrderModel, 'sync', this.handleCancelOrder);
+        this.listenTo(this.serviceDetailModel, 'sync', this.handleServiceOrder);
         this.model.on('change',function () {
             that.render();
         });
@@ -47,16 +52,39 @@ var orderView = Backbone.View.extend({
         this.initRender();
     },
     toPay: function (e) {
+        e.stopPropagation();
         var $this = $(e.target);
-            orderNum = $this.attr('attrOrderId');
+            orderNum = $this.attr('attrOrderId'),
+            orderClassify = $this.data('classify');
 
+        var list = this.model.toJSON().result.list;
+        var orderItem;
+
+        // this.jumpPay(orderNum);
+
+        // 如果是保证金充值订单，先判断该资源是否已经下架
+        if(orderClassify == 3) {
+            orderItem = _.find(list, function(item) {
+                return item.orderNum == orderNum
+            }) 
+
+            this.serviceDetailModel.set('orderNum', orderNum);
+            this.serviceDetailModel.fetch({
+                data: {
+                    id: orderItem.sourceDetail[0].resourceId
+                }
+            })
+        }else {
+            this.jumpPay(orderNum);
+        }
+    },
+    jumpPay: function(orderNum) {
         var param = {
             orderNum: orderNum
         };
         var base = new Base64;
         window.localStorage.setItem('orderInfo', base.encode(JSON.stringify(param)));
         location.href = 'pay.html';
-        e.stopPropagation();
     },
     cancelPay: function(e) {
         var $this = $(e.target);
@@ -108,6 +136,17 @@ var orderView = Backbone.View.extend({
         setTimeout(function() {
             this.reloadPage();
         }.bind(this), 1000);
+    },
+    handleServiceOrder: function() {
+        var model = this.serviceDetailModel.toJSON();
+
+        if(model.status == 'OK') {
+            if(model.result.status == 7) {
+                layer.alert('该资源已经下架了，不能支付');
+            }else {
+                this.jumpPay(model.orderNum);
+            }
+        }
     }
 });
 
